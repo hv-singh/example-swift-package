e28714dbeecbbf093b1c851b4d60bbed28fb0a26
Harsh Vardhan Singh <hvsingh@endor.ai>
2025-07-09 17:43:17 +0530

    Add new package dependency

--- Package.swift
+++ Package.swift
@@ -13,13 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ExampleSwift",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire", "ArgumentParser"]),
         .testTarget(
             name: "ExampleSwiftTests",
             dependencies: ["ExampleSwift"]
